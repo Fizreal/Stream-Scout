27cb6b73042af3d3b45cc202c5601c6c52fcfabc
Adam Buffett <adam_buffett@outlook.com>
2024-03-13 18:25:22 -0400

    Create types index and initial app pages

---
+++ src/app/details/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import { useSocket } from '@/context/SocketsContext'
+import { Content } from '@/types/index'
+import { useParams } from 'next/navigation'
+
+const ContentDetails = () => {
+  const socket = useSocket()
+  const { id } = useParams()
+  const [content, setContent] = useState<Content>(null)
+
+  useEffect(() => {
+    if (!socket) {
+      return
+    }
+
+    socket.emit('get content', { id }, (content: Content) => {
+      setContent(content)
+    })
+  }, [id])
+
+  return <div>{content ? <div></div> : <p>Loading...</p>}</div>
+}
+
+export default ContentDetails
---
+++ src/types/index.ts
@@ -0,0 +1,28 @@
+export type StreamingInfo = {
+  country: String
+  availability: {
+    addon: String
+    leaving: String
+    link: String
+    service: String
+    streamingType: String
+  }[]
+}
+
+export type Content = {
+  title: String
+  tmdbId: String
+  streamingInfo: StreamingInfo[]
+  genres: String[]
+  releaseYear: Number
+  streamingValidated:
+    | { lastUpdated: Date; status: Boolean }
+    | { status: Boolean }
+  type: String
+  runtime: Number
+  backdrop: String
+  poster: String
+  overview: String
+  rating: Number
+  _id: String
+} | null
