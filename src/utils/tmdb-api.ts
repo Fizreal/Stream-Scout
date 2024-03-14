5e86c708793c60d5a1134e73fc90f955147c1531
Adam Buffett <adam_buffett@outlook.com>
2024-03-14 12:27:47 -0400

    Create tmdb-api endpoints

---
+++ src/utils/tmdb-api.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://api.themoviedb.org/3/'
+
+const TMDB_API = axios.create({ baseURL: BASE_URL })
+
+TMDB_API.interceptors.request.use(
+  (config) => {
+    config.headers[
+      'Authorization'
+    ] = `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+export const movieDetails = async (id: string) => {
+  const response = await TMDB_API.get(`movie/${id}`)
+  return response.data
+}
+
+export const showDetails = async (id: string) => {
+  const response = await TMDB_API.get(`tv/${id}`)
+  return response.data
+}
