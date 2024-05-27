34743993f5faa9a9ff4df01f95d775dffa6818bf
Adam Buffett <adam_buffett@outlook.com>
2024-05-27 10:29:00 -0400

    Update profile page with Suspense default

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 import WatchlistsSection from '@/components/profile/WatchlistsSection'
 import WatchedSection from '@/components/profile/WatchedSection'
@@ -30,72 +30,76 @@ const ProfilePage = () => {
   }, [])
 
   return (
-    <section className="w-full flex-flex-col">
-      <div className="relative grid grid-cols-3 bg-PrimaryDark h-12 w-full md:max-w-lg md:rounded-lg">
-        <button
-          onClick={() => {
-            setShowCreateModal(false)
-            handleDisplayChange('watchlists')
-          }}
-          className={
-            'z-10 w-full h-full' +
-            (display === 'watchlists'
-              ? ' sectionSelected'
-              : ' sectionNotSelected')
-          }
-        >
-          Watchlists
-        </button>
-        <button
-          onClick={() => {
-            setShowCreateModal(false)
-            handleDisplayChange('watched')
-          }}
-          className={
-            'z-10' +
-            (display === 'watched' ? ' sectionSelected' : ' sectionNotSelected')
-          }
-        >
-          Watched
-        </button>
-        <button
-          onClick={() => {
-            setShowCreateModal(false)
-            handleDisplayChange('liked')
-          }}
-          className={
-            'z-10' +
-            (display === 'liked' ? ' sectionSelected' : ' sectionNotSelected')
-          }
-        >
-          Liked
-        </button>
-        <div
-          className="absolute top-0 left-0 h-full w-1/3 transition-all duration-300 p-2"
-          style={{
-            transform: `translateX(${
-              displayTranslation[display]
-                ? displayTranslation[display] * 100
-                : 0
-            }%)`
-          }}
-        >
-          <div className="w-full h-full bg-BaseDark rounded"></div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <section className="w-full flex-flex-col">
+        <div className="relative grid grid-cols-3 bg-PrimaryDark h-12 w-full md:max-w-lg md:rounded-lg">
+          <button
+            onClick={() => {
+              setShowCreateModal(false)
+              handleDisplayChange('watchlists')
+            }}
+            className={
+              'z-10 w-full h-full' +
+              (display === 'watchlists'
+                ? ' sectionSelected'
+                : ' sectionNotSelected')
+            }
+          >
+            Watchlists
+          </button>
+          <button
+            onClick={() => {
+              setShowCreateModal(false)
+              handleDisplayChange('watched')
+            }}
+            className={
+              'z-10' +
+              (display === 'watched'
+                ? ' sectionSelected'
+                : ' sectionNotSelected')
+            }
+          >
+            Watched
+          </button>
+          <button
+            onClick={() => {
+              setShowCreateModal(false)
+              handleDisplayChange('liked')
+            }}
+            className={
+              'z-10' +
+              (display === 'liked' ? ' sectionSelected' : ' sectionNotSelected')
+            }
+          >
+            Liked
+          </button>
+          <div
+            className="absolute top-0 left-0 h-full w-1/3 transition-all duration-300 p-2"
+            style={{
+              transform: `translateX(${
+                displayTranslation[display]
+                  ? displayTranslation[display] * 100
+                  : 0
+              }%)`
+            }}
+          >
+            <div className="w-full h-full bg-BaseDark rounded"></div>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col items-center w-full p-4">
-        {display === 'watchlists' ? (
-          <WatchlistsSection setShowCreateModal={setShowCreateModal} />
-        ) : display === 'watched' ? (
-          <WatchedSection />
-        ) : (
-          <LikedSection />
+        <div className="flex flex-col items-center w-full p-4">
+          {display === 'watchlists' ? (
+            <WatchlistsSection setShowCreateModal={setShowCreateModal} />
+          ) : display === 'watched' ? (
+            <WatchedSection />
+          ) : (
+            <LikedSection />
+          )}
+        </div>
+        {showCreateModal && (
+          <CreateWatchlistModal closeModal={() => setShowCreateModal(false)} />
         )}
-      </div>
-      {showCreateModal && (
-        <CreateWatchlistModal closeModal={() => setShowCreateModal(false)} />
-      )}
-    </section>
+      </section>
+    </Suspense>
   )
 }
 
