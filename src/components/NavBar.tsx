45413a9956ccc14fbfe6721fad92a511c73a7f0d
Adam Buffett <adam_buffett@outlook.com>
2024-05-30 10:22:14 -0400

    Fix issue with hamburger rendering before the user logs in on mobile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,17 +14,19 @@ const NavBar = ({
 
   return (
     <nav className="fixed top-0 left-0 bg-PrimaryDark flex justify-center items-center md:justify-start shadow-lg h-14 w-full z-20">
-      <div
-        ref={hamburgerRef}
-        className={`hamburgerMenu ${open ? 'open' : ''}`}
-        onClick={toggleOpen}
-        aria-label={open ? 'Close side bar' : 'Open side bar'}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+      {user && (
+        <div
+          ref={hamburgerRef}
+          className={`hamburgerMenu ${open ? 'open' : ''}`}
+          onClick={toggleOpen}
+          aria-label={open ? 'Close side bar' : 'Open side bar'}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      )}
       <Link
         href={user ? '/' : '/login'}
         className="flex items-center justify-center h-full w-[200px]"
